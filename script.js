5565d5add00ae03ec554e54d9266039df16d78b4
aaivey22 <angelawings22@gmail.com>
2020-03-01 19:47:27 -0500

    built start quiz button function and was success

--- script.js
+++ script.js
@@ -18,11 +18,40 @@
 * Go back button fires quiz function from beginning
 */
 
+var quiz = [
+    [1, "question1", "option1", "option2", "option3", "option4"], 
+    [2, "question2", "option1", "option2", "option3", "option4"],
+    [3, "question3", "option1", "option2", "option3", "option4"],
+    [4, "question4", "option1", "option2", "option3", "option4"],
+    [5, "question5", "option1", "option2", "option3", "option4"]
+    ];
+
+// var question1 = ["option1", "option2", "option3", "option4"]
+// var question2 = [option1, option2, option3, option4]
+// var question3 = [option1, option2, option3, option4]
+// var question4 = [option1, option2, option3, option4]
+// var question5 = [option1, option2, option3, option4]
+
 
 var button = document.getElementById('start');
 
 button.addEventListener('click', start);
 
+
 function start() {
-console.log("start now");
-}
+document.getElementById("questions").value = quiz;
+   console.log("start now"); 
+    
+}   
+    
+    
+    // for (var i = 0; questions > 4; i++) { // This for loop will iterate as many times as designated by the pwrdlength variable
+    //     // Password is called and concatenated with the selectedCase string where a golbal math object is then used to select a random character within the specified index string
+        
+    // }
+
+
+
+
+
+// document.write(i);
