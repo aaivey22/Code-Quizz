840e9ad532ba963840e45bfde5b2837d38035d2c
aaivey22 <angelawings22@gmail.com>
2020-03-04 01:22:12 -0500

    creating object array in js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
     click eListener for start button
     60 sec timer function starts
 * 5 questions
-    will be in For loop
+    replace content of element to display questions/answers
+    will be 2 For loops that do not run simultaneously
     click eventListener for correct answer button
     click eventListener for incorrect answer buttons
     60 sec timer with while loop
@@ -15,22 +16,41 @@
         Else continue question loop
 * High Score Pg
     prevent default to stop loop
-* Go back button fires quiz function from beginning
+* Go back button fires quiz function from beginning.
+    Try using query selectors and make the nested arrays an object
 */
-
+// var list = [
+//     {
+//         name: '',
+//         possible: [],
+//         ...
+//     },
+//     {},
+//     {},
+//     {},
+// ]
 var quiz = [
-    [1, "question1", "option1", "option2", "option3", "option4"], 
-    [2, "question2", "option1", "option2", "option3", "option4"],
-    [3, "question3", "option1", "option2", "option3", "option4"],
-    [4, "question4", "option1", "option2", "option3", "option4"],
-    [5, "question5", "option1", "option2", "option3", "option4"]
-    ];
-
-// var question1 = ["option1", "option2", "option3", "option4"]
-// var question2 = [option1, option2, option3, option4]
-// var question3 = [option1, option2, option3, option4]
-// var question4 = [option1, option2, option3, option4]
-// var question5 = [option1, option2, option3, option4]
+    {
+        question1: "what is thing?",
+        possibleA1: ["A","B","C"],
+        correctA1: "D"
+    },
+    {
+        question2: "what is thing?",
+        possibleA2: ["A","B","C"],
+        correctA2: "D"
+    },
+    {
+        question3: "what is thing?",
+        possibleA3: ["A","B","C"],
+        correctA3: "D"
+    },
+    {
+        question4: "what is thing?",
+        possibleA4: ["A","B","C"],
+        correctA3: "D"
+    },
+]
 
 
 var button = document.getElementById('start');
@@ -39,19 +59,21 @@ button.addEventListener('click', start);
 
 
 function start() {
-document.getElementById("questions").value = quiz;
-   console.log("start now"); 
+    
+    for (var i = 0; i <= quiz.length; i++) {
+      document.querySelector("quiz").textContent = quiz; 
+      console.log("start now"); 
+    //   document.getElementById("questions").innerHTML = quiz; use this to replace content in container
+    }
     
 }   
-    
-    
-    // for (var i = 0; questions > 4; i++) { // This for loop will iterate as many times as designated by the pwrdlength variable
-    //     // Password is called and concatenated with the selectedCase string where a golbal math object is then used to select a random character within the specified index string
-        
-    // }
 
-
-
+// This for loop will iterate as many times as designated by the pwrdlength variable
+// Password is called and concatenated with the selectedCase string where a golbal math object is then used to select a random character within the specified index string
 
 
 // document.write(i);
+
+
+
+
